95d5ef99d1cb19664667fde26a0c9b3f4477a49e
Igor Martayan <igor@martayan.org>
2024-06-17 16:03:20 +0200

    Update mod, add tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
+pub mod algorithm;
 mod builder;
-mod iterator;
+pub mod iterator;
 
 pub use builder::MinimizerBuilder;
-pub use iterator::{MinimizerIterator, MinimizerPosIterator};
+pub use minimizer_queue::DefaultHashBuilder;
 
 #[cfg(test)]
 mod tests {
@@ -44,6 +45,44 @@ mod tests {
         assert_eq!(min_iter.next(), Some(6)); // CAC
         assert_eq!(min_iter.next(), Some(7)); // ACA
         assert_eq!(min_iter.next(), Some(9)); // AAT
+        assert_eq!(min_iter.next(), None);
+    }
+
+    #[test]
+    fn test_mod_minimizer_iter() {
+        let seq = b"TGATTGCACAATC";
+        let minimizer_size = 4;
+        let width = 4;
+        let hasher = BuildNoHashHasher::<u64>::default();
+        let mut min_iter = MinimizerBuilder::new_mod()
+            .minimizer_size(minimizer_size)
+            .width(width)
+            .hasher(hasher)
+            .iter(seq);
+
+        assert_eq!(min_iter.next(), Some((0b00111110, 2))); // ATTG
+        assert_eq!(min_iter.next(), Some((0b01000100, 6))); // CACA
+        assert_eq!(min_iter.next(), Some((0b00010000, 7))); // ACAA
+        assert_eq!(min_iter.next(), Some((0b00001101, 9))); // AATC
+        assert_eq!(min_iter.next(), None);
+    }
+
+    #[test]
+    fn test_mod_minimizer_iter_pos() {
+        let seq = b"TGATTGCACAATC";
+        let minimizer_size = 4;
+        let width = 4;
+        let hasher = BuildNoHashHasher::<u64>::default();
+        let mut min_iter = MinimizerBuilder::<u64, _>::new_mod()
+            .minimizer_size(minimizer_size)
+            .width(width)
+            .hasher(hasher)
+            .iter_pos(seq);
+
+        assert_eq!(min_iter.next(), Some(2)); // ATTG
+        assert_eq!(min_iter.next(), Some(6)); // CACA
+        assert_eq!(min_iter.next(), Some(7)); // ACAA
+        assert_eq!(min_iter.next(), Some(9)); // AATC
         assert_eq!(min_iter.next(), None);
     }
 }
